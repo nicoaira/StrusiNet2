ecd864dfb44e3d71b00dc39b8fd55e6146e7b5fa
Nicolas <aira.nicolas94@gmail.com>
2024-10-10 16:24:38 +0200

    fixed strusinet test

--- tests/test_strusinet.py
+++ tests/test_strusinet.py
@@ -6,16 +6,16 @@ import unittest
 class TestStrusiNet(unittest.TestCase):
     def setUp(self):
         # Setup paths
-        self.input_csv = 'example_data/sample_dataset.csv'
-        self.output_csv = 'example_data/sample_dataset_with_embeddings.csv'
+        self.input = 'example_data/sample_dataset.csv'
+        self.output = 'example_data/sample_dataset_with_embeddings.tsv'
         self.model_path = 'saved_model/ResNet-Secondary.pth'
     
     def test_generate_embeddings(self):
         # Run the strusinet script using subprocess
         command = [
             'python', 'strusinet.py',
-            '--input_csv', self.input_csv,
-            '--output_csv', self.output_csv,
+            '--input', self.input,
+            '--output', self.output,
             '--structure_column_num', '6',  # Since we know secondary structure is in column 6
             '--header', 'True', 
             '--model_path', self.model_path,
@@ -28,16 +28,16 @@ class TestStrusiNet(unittest.TestCase):
         self.assertEqual(result.returncode, 0, f"Script failed with error: {result.stderr}")
 
         # Check that the output CSV exists
-        self.assertTrue(os.path.exists(self.output_csv), "Output CSV file was not created.")
+        self.assertTrue(os.path.exists(self.output), "Output CSV file was not created.")
         
         # Load the output CSV and check for 'embedding_vector' column
-        df = pd.read_csv(self.output_csv)
+        df = pd.read_csv(self.output)
         self.assertIn('embedding_vector', df.columns, "Output CSV does not contain 'embedding_vector' column.")
 
     def tearDown(self):
         # Clean up generated output file
-        if os.path.exists(self.output_csv):
-            os.remove(self.output_csv)
+        if os.path.exists(self.output):
+            os.remove(self.output)
 
 if __name__ == '__main__':
     unittest.main()
