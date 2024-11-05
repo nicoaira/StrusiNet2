cf577bc97e458daed629575420f6aedae832eb3c
Mercedes Castro <meki.castro@gmail.com>
2024-11-05 14:51:48 -0300

    add early stopping class

---
+++ src/model/early_stopping.py
@@ -0,0 +1,24 @@
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0):
+        """
+        Early stops the training if validation loss doesn't improve after a given patience.
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+            min_delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+        elif val_loss > self.best_loss - self.min_delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = val_loss
+            self.counter = 0
