8df143b7116d05140a73aedee5c0e6fe221f424f
Mercedes Castro <meki.castro@gmail.com>
2024-11-05 23:55:46 -0300

    Add triplet_loss class

---
+++ src/triplet_loss.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+        self.loss_fn = nn.TripletMarginLoss(margin=margin, p=2)
+
+    def forward(self, anchor, positive, negative):
+        return self.loss_fn(anchor, positive, negative)
