72ed345bfaaec230aedd50d3fe718ef6c50d0408
Mercedes Castro <meki.castro@gmail.com>
2024-11-14 19:13:23 +0100

    add graph encoding parameter to accomodate for forgi encoding

--- src/gin_rna_dataset.py
+++ src/gin_rna_dataset.py
@@ -1,9 +1,10 @@
 from torch.utils.data import Dataset
-from src.utils import dotbracket_to_graph, graph_to_tensor
+from src.utils import dotbracket_to_forgi_graph, dotbracket_to_graph, forgi_graph_to_tensor, graph_to_tensor
 
 class GINRNADataset(Dataset):
-    def __init__(self, dataframe):
+    def __init__(self, dataframe, graph_encoding = "allocator"):
         self.dataframe = dataframe
+        self.graph_encoding = graph_encoding
 
     def __len__(self):
         return len(self.dataframe)
@@ -13,12 +14,22 @@ class GINRNADataset(Dataset):
         positive_structure = self.dataframe.iloc[idx]["structure_P"]
         negative_structure = self.dataframe.iloc[idx]["structure_N"]
 
-        g_anchor = dotbracket_to_graph(anchor_structure)
-        g_positive = dotbracket_to_graph(positive_structure)
-        g_negative = dotbracket_to_graph(negative_structure)
+        if self.graph_encoding == "allocator":
+            g_anchor = dotbracket_to_graph(anchor_structure)
+            g_positive = dotbracket_to_graph(positive_structure)
+            g_negative = dotbracket_to_graph(negative_structure)
 
-        data_anchor = graph_to_tensor(g_anchor)
-        data_positive = graph_to_tensor(g_positive)
-        data_negative = graph_to_tensor(g_negative)
+            data_anchor = graph_to_tensor(g_anchor)
+            data_positive = graph_to_tensor(g_positive)
+            data_negative = graph_to_tensor(g_negative)
+        
+        elif self.graph_encoding == "forgi":
+            g_anchor = dotbracket_to_forgi_graph(anchor_structure)
+            g_positive = dotbracket_to_forgi_graph(positive_structure)
+            g_negative = dotbracket_to_forgi_graph(negative_structure)
+
+            data_anchor = forgi_graph_to_tensor(g_anchor)
+            data_positive = forgi_graph_to_tensor(g_positive)
+            data_negative = forgi_graph_to_tensor(g_negative)
         
         return data_anchor, data_positive, data_negative
