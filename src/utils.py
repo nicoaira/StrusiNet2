83bb931d8b74bbec2fcfbd0109909eca75a04bdd
Mercedes Castro <meki.castro@gmail.com>
2024-12-03 13:14:11 +0100

    update representation of graph to 1

--- src/utils.py
+++ src/utils.py
@@ -135,7 +135,7 @@ def dotbracket_to_graph(dotbracket):
 
 
 def graph_to_tensor(g):
-    x = torch.Tensor([[0, 1] if g.nodes[node]['label'] == 'unpaired' else [1, 0] for node in g.nodes])
+    x = torch.Tensor([[0] if g.nodes[node]['label'] == 'unpaired' else [1] for node in g.nodes])
     edge_index = torch.LongTensor(list(g.edges())).t().contiguous()
 
     # Graph to Data object
