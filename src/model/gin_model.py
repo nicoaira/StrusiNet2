bcb421247d34b5dc501e19d993626b787fd00654
Mercedes Castro <meki.castro@gmail.com>
2024-12-13 16:49:51 +0100

    fixes on GIN layers iterations

--- src/model/gin_model.py
+++ src/model/gin_model.py
@@ -2,23 +2,33 @@ import torch.nn as nn
 from torch_geometric.nn import global_add_pool
 from torch_geometric.nn import GINConv, Set2Set
 
-class GINModel(nn.Module):
+class GINModelGeneral(nn.Module):
     def __init__(self, hidden_dim, output_dim, graph_encoding = "allocator", gin_layers=1, dropout=0.1):
-        super(GINModel, self).__init__()
+        super(GINModelGeneral, self).__init__()
 
         input_dim = 1 if graph_encoding == "allocator" else 7
 
-        # Define GIN MLP 
-        self.convs = nn.ModuleList([
-            GINConv(nn.Sequential(
-                nn.Linear(input_dim, hidden_dim),
-                nn.ReLU(),
-                nn.Linear(hidden_dim, hidden_dim)
-            )) for _ in range(gin_layers)
-        ])
+        # Define GIN MLP
+        convs = []
+        for i in range(gin_layers):
+            if i == 0:
+                net = nn.Sequential(
+                    nn.Linear(input_dim, hidden_dim),
+                    nn.ReLU(),
+                    nn.Linear(hidden_dim, hidden_dim)
+                )
+            else:
+                net = nn.Sequential(
+                    nn.Linear(hidden_dim, hidden_dim),
+                    nn.ReLU(),
+                    nn.Linear(hidden_dim, hidden_dim)
+                )
+            convs.append(GINConv(net))
+        
+        self.convs = nn.ModuleList(convs)
        
         # Define pooling layer option
-        self.pooling = global_add_pool()
+        self.pooling = global_add_pool
         ## self.pooling = Set2Set(hidden_dim, processing_steps=10)
 
         # Dropout layer (optional to avoid overfitting)
@@ -33,8 +43,8 @@ class GINModel(nn.Module):
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
         # Pass through GIN convolution layer
-        for i, con in enumerate(self.convs):
-            x = con(x, edge_index)
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
             #x = self.dropout(x)
 
         # Pooling for graph-level embedding
