b240b63c46b4decc1714feda9698b4de72cb7ee5
Mercedes Castro <meki.castro@gmail.com>
2025-01-12 16:40:27 +0100

    adapt script for optuna optimiser

--- train_model_optuna.py
+++ train_model_optuna.py
@@ -1,3 +1,4 @@
+import argparse
 import optuna
 import os
 import torch
@@ -86,68 +87,71 @@ def train_model_with_early_stopping(
     save_model_to_local(model, optimizer, epoch, output_name)
     return val_loss / len(val_loader)
 
-def objective(trial):
-    # Sample hyperparameters from the search space
-    lr = trial.suggest_loguniform('lr', 1e-5, 1e-2)
-    hidden_dim = trial.suggest_int('hidden_dim', 128, 256, 512)
-    output_dim = trial.suggest_int('output_dim', 32, 64, 128)
-    gin_layers = trial.suggest_int('gin_layers', 1, 2, 3)
-    batch_size = trial.suggest_categorical('batch_size', [8, 16, 32])
-    num_epochs = trial.suggest_int('num_epochs', 5, 15)
-    patience = trial.suggest_int('patience', 3, 7)
-
-    # Load data
-    dataset_path = 'data/rna_structures.csv'  # Replace with the actual path
-    df = pd.read_csv(dataset_path)
-    df = remove_invalid_structures(df)
-    train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
-
-    # Choose the model type
-    model_type = 'siamese'  # or 'gin_1', 'gin'
-    graph_encoding = 'allocator' # or 'forgi'
-    
-    # Instantiate model based on hyperparameters
-    if model_type == "siamese":
-        model = SiameseResNetLSTM(input_channels=1, hidden_dim=hidden_dim, lstm_layers=1)
-        train_dataset = TripletRNADataset(train_df, max_len=max(df['structure_A'].str.len()))
-        val_dataset = TripletRNADataset(val_df, max_len=max(df['structure_A'].str.len()))
-        train_loader = TorchDataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-        val_loader = TorchDataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    
-    elif model_type == "gin_1":
-        model = GINModelSingleLayer(graph_encoding=graph_encoding, hidden_dim=hidden_dim, output_dim=output_dim)
-        train_dataset = GINRNADataset(train_df, graph_encoding=graph_encoding)
-        val_dataset = GINRNADataset(val_df, graph_encoding=graph_encoding)
-        train_loader = GeoDataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-        val_loader = GeoDataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
-
-    elif model_type == "gin":
-        model = GINModel(hidden_dim=hidden_dim, output_dim=output_dim, graph_encoding=graph_encoding, gin_layers = gin_layers)
-        train_dataset = GINRNADataset(train_df, graph_encoding=graph_encoding)
-        val_dataset = GINRNADataset(val_df, graph_encoding=graph_encoding)
-        train_loader = GeoDataLoader(train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-        val_loader = GeoDataLoader(val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
-
-    # Set up criterion and optimizer
-    criterion = TripletLoss(margin=1.0)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-
-    # Run training with early stopping and return the validation loss
-    val_loss = train_model_with_early_stopping(
-        model,
-        'optuna_model',
-        train_loader,
-        val_loader,
-        optimizer,
-        criterion,
-        num_epochs=num_epochs,
-        patience=patience,
-        device='cpu'
-    )
-    return val_loss
-
 def main():
+
+    parser = argparse.ArgumentParser(description="Generate embeddings from RNA secondary structures using a trained Siamese or GIN model.")
+    parser.add_argument('--input_path', type=str, required=True, help='Path to the input CSV/TSV file containing RNA secondary structures.')
+    parser.add_argument('--model_type', type=str, default='gin', required=True, choices=['siamese', 'gin_1', 'gin'], help="Type of model to use: 'siamese' or 'gin'.")
+    parser.add_argument('--graph_encoding', type=str, choices=['allocator', 'forgi'], default='allocator', help='Encoding to use for the transformation to graph. Only used in case of gin modeling')
+    parser.add_argument('--batch_size', type=int, default=100, help='Batch size for training and validation.')
+    parser.add_argument('--num_epochs', type=int, default=4, help='Number of epochs to train the model.')
+    parser.add_argument('--patience', type=int, default=5, help='Patience for early stopping.')
+    args = parser.parse_args()
     # Create an Optuna study to optimize hyperparameters
+
+    
+    def objective(trial):
+        # Sample hyperparameters from the search space
+        lr = trial.suggest_float('lr', 1e-5, 1e-2, log=True)
+        hidden_dim = trial.suggest_categorical('hidden_dim', [128, 256, 512])
+        output_dim = trial.suggest_categorical('output_dim', [32, 64, 128])
+        gin_layers = trial.suggest_categorical('gin_layers', [1, 2, 3, 4, 5])
+
+        # Load data
+        df = pd.read_csv(args.input_path)
+        df = remove_invalid_structures(df)
+        train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
+        
+        # Instantiate model based on hyperparameters
+        if args.model_type == "siamese":
+            model = SiameseResNetLSTM(input_channels=1, hidden_dim=hidden_dim, lstm_layers=1)
+            train_dataset = TripletRNADataset(train_df, max_len=max(df['structure_A'].str.len()))
+            val_dataset = TripletRNADataset(val_df, max_len=max(df['structure_A'].str.len()))
+            train_loader = TorchDataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+            val_loader = TorchDataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
+        
+        elif args.model_type == "gin_1":
+            model = GINModelSingleLayer(graph_encoding=args.graph_encoding, hidden_dim=hidden_dim, output_dim=output_dim)
+            train_dataset = GINRNADataset(train_df, graph_encoding=args.graph_encoding)
+            val_dataset = GINRNADataset(val_df, graph_encoding=args.graph_encoding)
+            train_loader = GeoDataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+            val_loader = GeoDataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True)
+
+        elif args.model_type == "gin":
+            model = GINModel(hidden_dim=hidden_dim, output_dim=output_dim, graph_encoding=args.graph_encoding, gin_layers = gin_layers)
+            train_dataset = GINRNADataset(train_df, graph_encoding=args.graph_encoding)
+            val_dataset = GINRNADataset(val_df, graph_encoding=args.graph_encoding)
+            train_loader = GeoDataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+            val_loader = GeoDataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, pin_memory=True)
+
+        # Set up criterion and optimizer
+        criterion = TripletLoss(margin=1.0)
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+
+        # Run training with early stopping and return the validation loss
+        val_loss = train_model_with_early_stopping(
+            model,
+            'optuna_model',
+            train_loader,
+            val_loader,
+            optimizer,
+            criterion,
+            num_epochs=args.num_epochs,
+            patience=args.patience,
+            device="cuda" if torch.cuda.is_available() else "cpu"
+        )
+        return val_loss
+
     study = optuna.create_study(direction='minimize')
     study.optimize(objective, n_trials=50)  # Run 50 trials
 
